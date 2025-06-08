3b308fc31e686ee99485012d8a16c39ef2a1f222
jerrysamuel81 <jerrysamuel81@gmail.com>
2025-06-08 04:51:32 +0100

    changed db to production

--- database.py
+++ database.py
@@ -4,11 +4,17 @@ from sqlalchemy.ext.declarative import declarative_base
 from dotenv import load_dotenv
 import os
 
-
 load_dotenv()
+
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+# Use connect_args only for SQLite
+connect_args = {}
+if DATABASE_URL.startswith("sqlite"):
+    connect_args = {"check_same_thread": False}
+
+engine = create_engine(DATABASE_URL, connect_args=connect_args)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 
 Base = declarative_base()
